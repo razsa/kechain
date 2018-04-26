3b3f3aa017a360ea2fe1e352feb99161e34aaa46
vagrant <vagrant@vagrant.vm>
2018-04-26 21:48:47 +0000

    first commit of blockchain class

---
+++ blockchain.py
@@ -0,0 +1,21 @@
+class Blockchain(object):
+
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    def new_block(self):
+        # Creates a new Block and adds it to the chain
+        pass
+
+    def new_transaction(self):
+        # Adds a new transaction to the list of transactions
+        pass
+
+    @staticmehod
+    def hash(block):
+        # Hashes a Block
+        pass
+
+    @property
+    def last_block(self):
