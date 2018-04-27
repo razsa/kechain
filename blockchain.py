dd22bcf46fc967c906ef0c38eca473dc851ed63e
razsa <[kamil@elwawi.com]>
2018-04-27 19:13:23 -0400

    Create a SHA256 hash of a block

--- blockchain.py
+++ blockchain.py
@@ -56,7 +56,16 @@ class Blockchain(object):
 
     @staticmehod
     def hash(block):
-        # Hashes a Block
+        """
+        Creates a SHA-256 hash of a Block
+
+        :param block: <dict> Block
+        :return: <str>
+        """
+        # We must make sure that the Dictionary is Ordered, or we'll have
+        # inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
         pass
 
     @property
